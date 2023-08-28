c745f1a04c2a5ead07850bd6fa094b18c5993027
Dher82 <126974687+Dher82@users.noreply.github.com>
2023-08-28 23:40:30 +0000

    conditional-Card-dhc

--- src/app.js
+++ src/app.js
@@ -2,10 +2,51 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+  // console.log("Hello Rigo from the console!");
+  document.querySelector("#Boton_Cartas").addEventListener("click", () => {
+    document.querySelector(".card").innerHTML = generateRandomNumber();
+  });
+  document.querySelector("#card-width").addEventListener("change", e => {
+    document.querySelector("#card").style.width = e.target.value + "px";
+  });
+  document.querySelector("#card-height").addEventListener("change", e => {
+    document.querySelector("#card").style.height = e.target.value + "px";
+  });
 };
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  document
+    .querySelector(".card")
+    .setAttribute("class", "card " + generateRandomSuit());
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+// window.redimensionar = function redimensionar() {
+//   console.log("hola");
+// };
